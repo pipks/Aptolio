182006f430116bd3ef61854eee4d91c67de6864d
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-22 16:03:54 +0400

    update Button design

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -8,7 +8,7 @@ const Index = ({ color, textColor, contained, children, className = '', ...rest 
 				(
 					<button
 						{...rest}
-						className={`border-[1px] border-${color} px-4 py-2 text-primary hover:text-white rounded-lg duration-150 hover:scale-[0.99] w-full hover:bg-${color} shadow-lg hover:shadow-${color}/40 disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed disabled:scale-100 disabled:border-0 ${className}`}>
+						className={`border-[1px] border-darkButton px-4 py-2 text-darkButton hover:text-black rounded-lg duration-150 w-full hover:bg-darkButton disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed disabled:border-0 ${className}`}>
 						{children}
 					</button>
 				)
@@ -16,7 +16,7 @@ const Index = ({ color, textColor, contained, children, className = '', ...rest 
 				(
 					<button
 						{...rest}
-						className={`bg-${color} px-4 py-2 text-${textColor} rounded-lg duration-150 hover:scale-[0.99] hover:bg-primaryDark shadow-lg w-full hover:shadow-${color}/40 disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed disabled:scale-100 ${className}`}>
+						className={`bg-darkButton px-4 py-2 w-full rounded-lg duration-200 hover:bg-darkButton/70 disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed ${className}`}>
 						{children}
 					</button>
 				)}
@@ -25,7 +25,7 @@ const Index = ({ color, textColor, contained, children, className = '', ...rest 
 }
 
 Index.defaultProps = {
-	color: 'primary',
+	color: 'darkButton',
 	textColor: 'white',
 };
 
