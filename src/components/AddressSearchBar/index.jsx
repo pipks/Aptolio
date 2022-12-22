31dc46a9b710c191ed68aa34624a415f8d36c502
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-23 00:02:12 +0400

    Added "CHECK ME" button

--- src/components/AddressSearchBar/index.jsx
+++ src/components/AddressSearchBar/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import Input from 'components/Input'
+import { useWallet } from '@aptos-labs/wallet-adapter-react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { useToast } from 'hooks/useToast'
 import { convertNameToAddress } from 'utils/APIs/AptosAPI'
 
 const Index = () => {
   const toast = useToast()
+  const { account, connected } = useWallet()
 
-  const checkAddress = async () => {
-    const address = document.getElementById('aptosAddress').value
-    if (address !== '') {
-      if (String(address).includes('.apt')) {
-        const getAddress = await convertNameToAddress(address)
+  const checkAddress = async (walletAddress) => {
+    if (walletAddress !== '') {
+      if (String(walletAddress).includes('.apt')) {
+        const getAddress = await convertNameToAddress(walletAddress)
         if (getAddress.status === 200 && getAddress.data.hasOwnProperty('address')) {
           window.location = `/wallet/${getAddress.data.address}`
         } else {
           toast('error', 'Not Found', 'Invalid or not registered')
         }
-      } else if (String(address).length > 60) {
-        window.location = `/wallet/${address}`
+      } else if (String(walletAddress).length > 60) {
+        window.location = `/wallet/${walletAddress}`
       } else {
         toast('error', 'Wallet address or .apt name is incorrect')
       }
@@ -30,11 +31,17 @@ const Index = () => {
   return (
     <div className='w-full md:w-[450px]'>
       <div className='flex items-center gap-1'>
-        <Input onKeyDown={e => e.key === 'Enter' && checkAddress()} id='aptosAddress' placeholder='APTOS Wallet Address or .apt name' />
+        <Input onKeyDown={e => e.key === 'Enter' && checkAddress(document.getElementById('aptosAddress').value)} id='aptosAddress' placeholder='APTOS Wallet Address or .apt name' />
         <div onClick={() => checkAddress()} className='group border-[1px] border-darkBorder p-2 rounded-lg cursor-pointer duration-200 hover:bg-primary'>
           <AiOutlineSearch className='duration-200 text-2xl text-primary  group-hover:text-darkBackground' />
         </div>
+        {connected && (
+          <div onClick={() => checkAddress(account.address)} className='group border-[1px] border-darkBorder p-2 rounded-lg cursor-pointer duration-200 hover:bg-primary'>
+            <p className='duration-200 text-primary group-hover:text-darkBackground text-md whitespace-nowrap'>CHECK ME</p>
+          </div>
+        )}
       </div>
+
     </div>
   )
 }
